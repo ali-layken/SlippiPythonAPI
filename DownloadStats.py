ebdb366f328f7fe39bfb8a1508796a69a9395634
Layken <ausman99@gmail.com>
2017-04-24 22:28:14 -0400

    Add files via upload

---
+++ DownloadStats.py
@@ -0,0 +1,95 @@
+#Imports
+import urllib 
+import json
+import sys
+import pickle
+
+
+#Functions & Classes
+#Misc. functions
+def DownloadSetData(setNums):
+	"Returns a list. Every item is a json dict containing info about a set"
+	setJSONList = []
+	for s in setNums:
+		setJSON = json.loads(urllib.urlopen('https://api.smash.gg/slippi/getBySet/' + s).read())
+		if 'games' in setJSON:
+			setJSONList.append(setJSON)
+		else:
+			print  "There is no data for set #{}".format(s)
+	return setJSONList
+
+def DownloadPhaseData(phase):
+	"Returns a list containing all the set #s in a smash.gg phase"
+	return [str(s['id']) for s in json.loads(urllib.urlopen('https://api.smash.gg/phase_group/{}?expand[]=sets'.format(phase)).read())['entities']['sets']]
+
+
+
+#Extract data from JSON
+def ExtractPlayerIDs(set):
+	"Pass in a set(json), returns list of playerIDs in the set"
+	return [int(id) for id in set['summary']]
+
+def ExtractGameInfo(setData):
+	data = {}
+	for s in setData:
+		playerIdList = ExtractPlayerIDs(s)
+		for game in s['games']:
+			data[game] = {'stage' : s['games'][game]['stage'], 'p1' : playerIdList[0], 'p2' : playerIdList[1]}
+			for player in s['games'][game]['players']:
+				if player['playerId'] == playerIdList[0]:
+					data[game]['p1Char'] = player['sCharacterId']
+				else:
+					data[game]['p2Char'] = player['sCharacterId']
+	return data
+
+
+
+#Convert/Analyze extracted data functions
+def CalculatePreHitPercent(move, killer, victimFinalPercent):
+	"Takes a move char and percent after hit and returns percent before hit"
+	return None
+
+def ConvertMove(id):
+	idList = {1 : 'Turnip?', 2 : 'Jab 1', 3 : 'Jab 2', 4 : 'Jab 3', 6 : 'Dash Attack', 7: 'Ftilt', 8 : 'Utilt', 9 : 'Dtilt', 10 : 'Fsmash', 11 : 'Usmash', 12 : 'Dsmash', 13 : 'Nair', 14 : 'Fair', 15 : 'Bair', 16 : 'Uair', 17 : 'Dair', 18 : 'Nspecial', 19 : 'Fspecial', 20 : 'Uspecial', 21 : 'Dspecial', 52 : 'Dthrow', 53 : 'Fthrow', 54 : 'Bthrow', 55 : 'Uthrow'}
+	return(idList[id])
+
+def ConvertPlayerId(id):
+	idList = {1012 : 'PewPewU', 1017 : 'S2J', 4465 : 'Leffen', 4101 : 'Infinite Numbers', 6189 : 'Armada', 13932 : 'Lucky', 1004 : 'Hungrybox', 1008 : 'Westballz', 1055 : 'Swedish Delight', 16342 : 'Axe', 1028 : 'Wizzrobe', 1037 : 'Duck', 1019 : 'SFAT', 4507 : 'The Moon', 4442 : 'Druggedfox', 1000 : 'Mang0', 15179 : 'ChuDat', 1003 : 'Mew2King', 1013 : 'Shroomed'}
+	return(idList[id])
+
+def ConvertCharacter(id):
+	idList = {0 : 'Captain Falcon', 2 : 'Fox', 7 : 'Luigi', 9 : 'Marth', 12 : 'Peach', 13 : 'Pikachu', 15 : 'Jigglypuff', 16 : 'Samus', 18 : 'Zelda', 19 : 'Sheik', 20 : 'Falco', 25 : 'Ganondorf', 14 : 'Ice Climbers'}
+	return(idList[id])
+
+def ConvertStage(id):
+	idList = {2 : 'Fountain of Dreams', 3 : 'Pokemon Stadium', 8 : "Yoshi's Story", 28 : 'Dreamland', 31 : 'Battlefield', 32 : 'Final Destination'}
+	return(idList[id])
+
+
+#Main
+def main():
+	#Main
+	#Handle command-line args
+	resultType = raw_input("Set or Phase [s/p]? ")
+	if resultType.lower() == 'p':
+		setNums = DownloadPhaseData(str(input('Enter the smash.gg phase #: ')))
+		print '\n\nDownloading stats for {} sets: {}\n\n'.format(len(setNums), setNums)
+	elif resultType.lower() == 's':
+		setNums = [str(raw_input('Enter the smash.gg set #: '))] 
+		if not setNums: print 'y tho'
+		print '\n\nDownloading stats for 1 set: {}\n\n'.format(setNums)
+	else:
+		setNums = ['7650196'] 
+		print '\n\nDownloading stats for 1 set: {}\n\n'.format(setNums)
+	setData = DownloadSetData(setNums)	#List containing all of the data about every set in setNums
+	gameInfo = ExtractGameInfo(setData)	#Takes a list of dicts with game info and returns a list of kills + info about kills
+	with open('DownloadedInfo.pkl', 'w+b') as p:
+		pickle.dump([setData, gameInfo], p, -1)
+	print '\n\nDone'
+	return None
+
+if __name__ == "__main__":
+	main()
+
+
+
---
+++ killAnalyze.py
@@ -0,0 +1,94 @@
+import pickle
+import DownloadStats
+from collections import Counter
+
+#Functions
+#Info Extraction Functions
+def ExtractKillInfo(setData, gameInfo):
+    "Returns a list containing kill objects when given a list of sets and game info"
+    kills = []
+    for s in setData:
+        for game in s['games']:
+            for player in s['games'][game]['players']:
+                victim = player['playerId']
+                victimChar = player['sCharacterId']
+                killer = gameInfo[game]['p1'] if not gameInfo[game]['p1'] == victim else gameInfo[game]['p2']
+                killerChar =  gameInfo[game]['p1Char'] if not gameInfo[game]['p1Char'] == victimChar else gameInfo[game]['p2Char']
+                for stock in player['stocks']:
+                    if not stock['frameEnd'] == 0 and not stock['moveLastHitBy'] == 0 and not stock['moveLastHitBy'] == 56:
+                        kills.append(Kill(stock['moveLastHitBy'], killer, victim, killerChar, victimChar, stock['percent'], gameInfo[game]['stage']))
+    return kills
+
+#Satistics Functions
+def AveragePerStage(kills):
+    percents = {}
+    for kill in kills:
+        if not kill.stage in percents:
+            percents[kill.stage] = [kill.percent]
+        else:
+            percents[kill.stage].append(kill.percent)
+    for key in percents:
+        percents[key] = int(round(sum(percents[key])/len(percents[key])))
+    return percents
+
+def MovePerPlayer(kills):
+    moves = {}
+    for kill in kills:
+        if not kill.killer in moves:
+            moves[kill.killer] = [kill.move]
+        else:
+            moves[kill.killer].append(kill.move)
+    for player in moves:
+        moves[player] = Counter(moves[player]).most_common(1)[0][0]
+    return moves
+
+def AveragePercentPerMovePerCharacter(char, kills):
+    percents = {}
+    for kill in kills:
+        if kill.killerChar == char.title():
+            if not kill.move in percents:
+                percents[kill.move] = [kill.percent]
+            else:
+                percents[kill.move].append(kill.percent)
+    for move in percents:
+        percents[move] =  int(round(sum(percents[move])/len(percents[move])))
+    return (char, percents)
+
+
+#Classes
+class Kill:
+    def __init__(self, move, killer, victim, killerChar, victimChar, victimFinalPercent, stage):
+        self.move = DownloadStats.ConvertMove(move)                                #Move used to kill
+        self.killer = DownloadStats.ConvertPlayerId(killer)                             #Killer Player Tag
+        self.victim = DownloadStats.ConvertPlayerId(victim)                            #Victim Player Tag    
+        self.killerChar = DownloadStats.ConvertCharacter(killerChar)                    #Killer's Character
+        self.victimChar = DownloadStats.ConvertCharacter(victimChar)                    #Victim's Character
+        self.stage = DownloadStats.ConvertStage(stage)
+        #self.victimPreHitPercent = CalculatePreHitPercent(move, killer, victimFinalPercent) #Victim's Percent Before Getting Hit
+        ##Implement this later^^
+        self.percent = victimFinalPercent
+        
+    def __str__(self):
+        #"Returns the the kill in the format: KillerTag(KillerChar) killed Victim(VictimChar) at %___"
+        s = "{}({}) killed {}({}) at %{} with {} on {}".format(self.killer, self.killerChar, self.victim, self.victimChar, self.percent, self.move, self.stage)
+        return s 
+    
+#Main Function
+def main():
+    ggInfo = pickle.load(open('DownloadedInfo.pkl', 'rb'))
+    kills = ExtractKillInfo(ggInfo[0], ggInfo[1]) #Get a list of all the kills in a list of sets
+    PercentxStage = AveragePerStage(kills)
+    MovexPlayer = MovePerPlayer(kills)
+    PercentxMovexChar = AveragePercentPerMovePerCharacter('fox', kills)
+    print 'Average death percent per stage:'
+    for stage in PercentxStage:
+        print '\t{0:>18}:  {1}%'.format(stage, PercentxStage[stage])
+    print '\n\n Favorite kill move per player:'
+    for player in MovexPlayer:
+        print '\t{0:>15}:  {1}'.format(player, MovexPlayer[player])
+    print '\n\n Average kill percentage per move for: Fox'
+    for move in PercentxMovexChar[1]:
+        print '\t{0:>10}:  {1}%'.format(move, PercentxMovexChar[1][move])
+    
+if __name__ == "__main__":
+    main()
